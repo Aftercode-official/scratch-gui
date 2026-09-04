1c766057637142414355ca1735cede20ecce329e
katboizz0-arch <katboizz0-arch@gmail.com>
2026-09-04 22:38:35 +0700

    gay

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -341,7 +341,7 @@ class Interface extends React.Component {
                                 </p>
                             </div>
                             <div className={styles.section}>
-                                <FeaturedProjects studio="51878970" />
+                                <FeaturedProjects studio="33509364" />
                             </div>
                         </React.Fragment>
                     ) : null}
