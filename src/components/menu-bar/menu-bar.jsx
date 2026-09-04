c0b5e8f05bb14039a96490091a9fd125fdb449f4
katboizz0-arch <katboizz0-arch@gmail.com>
2026-09-04 10:03:00 +0700

    fix

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -1043,7 +1043,7 @@ class MenuBar extends React.Component {
                             {/* todo: icon */}
                             <Button className={styles.feedbackButton}>
                                 <FormattedMessage
-                                    defaultMessage="Turbowarp Feedback"
+                                    defaultMessage="{APP_NAME} Feedback"
                                     description="Button to give feedback in the menu bar"
                                     id="tw.feedbackButton"
                                     values={{
